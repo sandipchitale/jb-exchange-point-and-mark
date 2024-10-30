58e157b0c7b86e6383b38581d3a19edbf9bb83e2
Sandip Chitale <sandipchitale@gmail.com>
2024-10-29 23:57:00 -0700

    Ask prefix and suffix separately.

--- src/main/java/sandipchitale/jbexchangepointandmark/SurroundWithPrefixSuffixAction.java
+++ src/main/java/sandipchitale/jbexchangepointandmark/SurroundWithPrefixSuffixAction.java
@@ -1,16 +1,19 @@
 package sandipchitale.jbexchangepointandmark;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Caret;
 import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.ui.DialogBuilder;
+import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.Computable;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
@@ -18,7 +21,12 @@ import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class SurroundWithPrefixSuffixAction extends AnAction {
+
+    public SurroundWithPrefixSuffixAction() {
+    }
+
     @Override
+    @SuppressWarnings("DialogTitleCapitalization")
     public void actionPerformed(@NotNull AnActionEvent actionEvent) {
         Editor editor = CommonDataKeys.EDITOR.getData(actionEvent.getDataContext());
         if (editor != null) {
@@ -26,29 +34,48 @@ public class SurroundWithPrefixSuffixAction extends AnAction {
             if (!document.isWritable()) {
                 Messages.showErrorDialog("The document is not writable!", "Error");
             } else {
-                @SuppressWarnings("DialogTitleCapitalization")
-                String prefixSuffix = Messages.showInputDialog(editor.getComponent(),
-                        "Specify [prefix]|[suffix]:",
-                        "[prefix]|[suffix]",
-                        AllIcons.Actions.ArrowExpand);
-                if (prefixSuffix != null && !prefixSuffix.isEmpty() && !prefixSuffix.equals("|")) {
-                    String[] prefixSuffixParts = prefixSuffix.split("\\|");
-                    String prefix = null;
-                    String suffix = null;
-                    if (prefixSuffixParts.length == 2) {
-                        prefix = prefixSuffixParts[0];
-                        suffix = prefixSuffixParts[1];
-                    } else if (prefixSuffixParts.length == 1) {
-                        prefix = prefixSuffixParts[0];
+
+                DialogBuilder builder = new DialogBuilder(editor.getComponent());
+
+                JPanel prefixSuffixPaneWrapper = new JPanel(new BorderLayout(10, 10));
+
+                JPanel prefixPanel = new JPanel(new BorderLayout(10, 10));
+                prefixPanel.add(new JLabel("Prefix:"), BorderLayout.WEST);
+                JTextField prefixTextField = new JTextField();
+                prefixTextField.setColumns(20);
+                prefixPanel.add(prefixTextField, BorderLayout.CENTER);
+                prefixSuffixPaneWrapper.add(prefixPanel, BorderLayout.NORTH);
+
+                JPanel suffixPanel = new JPanel(new BorderLayout(10, 10));
+                suffixPanel.add(new JLabel("Suffix:"), BorderLayout.WEST);
+                JTextField suffixTextField = new JTextField();
+                suffixTextField.setColumns(20);
+                suffixPanel.add(suffixTextField, BorderLayout.CENTER);
+                prefixSuffixPaneWrapper.add(suffixPanel, BorderLayout.SOUTH);
+
+                builder.setNorthPanel(prefixSuffixPaneWrapper);
+                builder.setCenterPanel(new JLabel("<html>Enter Prefix and Suffix. Use '(start, step)'<br/>in prefix, suffix strings to generate sequence."));
+
+                builder.setDimensionServiceKey("SurroundWithPrefixSuffix");
+                builder.setTitle("[prefix]|[suffix]");
+                builder.removeAllActions();
+                builder.addOkAction();
+                builder.addCancelAction();
+                builder.setPreferredFocusComponent(prefixTextField);
+
+                if (builder.show() == DialogWrapper.OK_EXIT_CODE) {
+                    String finalPrefix = prefixTextField.getText();
+                    String finalSuffix = suffixTextField.getText();
+                    if (finalPrefix.length() == 0 && finalSuffix.length() == 0) {
+                        Messages.showErrorDialog("Prefix and suffix are both zero length!\nMust specify at least one with non-zero length.", "Error");
+                        return;
                     }
-                    String finalPrefix = prefix;
-                    String finalSuffix = suffix;
                     WriteCommandAction.runWriteCommandAction(
                             editor.getProject(),
                             (Computable<String>) () -> {
                                 CaretModel caretModel = editor.getCaretModel();
                                 List<Caret> carets = caretModel.getAllCarets();
-                                boolean[] doNotCallExtraExchangeStartAndEndOfSelectionAction = { false };
+                                boolean[] doNotCallExtraExchangeStartAndEndOfSelectionAction = {false};
                                 IntStream.range(0, carets.size()).forEach((int index) -> {
                                     index = carets.size() - index - 1;
                                     Caret caret = carets.get(index);
@@ -67,10 +94,10 @@ public class SurroundWithPrefixSuffixAction extends AnAction {
                                         insertPrefixAt = selectionEnd;
                                         insertSuffixAt = selectionStart;
                                     }
-                                    if (finalSuffix != null) {
+                                    if (finalSuffix.length() != 0) {
                                         document.insertString(insertSuffixAt, process(finalSuffix, index));
                                     }
-                                    if (finalPrefix != null) {
+                                    if (finalPrefix.length() != 0) {
                                         document.insertString(insertPrefixAt, process(finalPrefix, index));
                                     }
                                 });
@@ -99,12 +126,13 @@ public class SurroundWithPrefixSuffixAction extends AnAction {
     }
 
     private static final Pattern stepSpecsPattern = Pattern.compile("\\(\\s*(\\d+)\\s*,\\s*(\\d+)\\s*\\)");
+
     private static String process(String prefixOrSuffix, int index) {
         Matcher matcher = stepSpecsPattern.matcher(prefixOrSuffix);
         return matcher.replaceAll((MatchResult match) -> {
             int start = Integer.parseInt(match.group(1));
             int step = Integer.parseInt(match.group(2));
-            return String.valueOf(start + index*step);
+            return String.valueOf(start + index * step);
         });
     }
 }
